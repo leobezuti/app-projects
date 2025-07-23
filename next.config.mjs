52ab0ffc96926fae85b0a80d6469f492da6a0e57
Leonardo Bezuti <leonardobezuti11@gmail.com>
2025-07-23 14:11:52 -0300

    Configuration to static deploy in S3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', 
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -7,7 +8,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
 }
 
